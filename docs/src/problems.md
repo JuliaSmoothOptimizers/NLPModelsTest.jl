0677983559f089f362e648f859848adeeb38e1ad
Abel <abel.s.siqueira@gmail.com>
2021-03-14 17:59:45 -0300

    Refactor TestUtils as a separate package

---
+++ docs/src/problems.md
@@ -0,0 +1,37 @@
+# Problems
+
+We have implemented a few problems to be used with the consistency checks, explained [here](@ref consistency).
+
+- NLP problems:
+  - [`BROWNDEN`](@ref)
+  - [`HS5`](@ref)
+  - [`HS6`](@ref)
+  - [`HS10`](@ref)
+  - [`HS11`](@ref)
+  - [`HS14`](@ref)
+  - [`LINCON`](@ref)
+  - [`LINSV`](@ref)
+  - [`MGH01Feas`](@ref)
+- NLS problems:
+  - [`LLS`](@ref)
+  - [`MGH01`](@ref)
+  - [`NLSHS20`](@ref)
+  - [`NLSLC`](@ref)
+
+## Description
+
+```@docs
+BROWNDEN
+HS5
+HS6
+HS10
+HS11
+HS14
+LINCON
+LINSV
+MGH01Feas
+LLS
+MGH01
+NLSHS20
+NLSLC
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,70 +1,44 @@
-include("TestUtils/TestUtils.jl")
-using .TestUtils
+using NLPModelsTest, Test
 
-using Test, NLPModels, LinearAlgebra, LinearOperators, Printf, SparseArrays
-
-@info("Testing printing of nlp.meta")
-print(IOBuffer(), ADNLPModel(x->0, zeros(10), [-ones(5); -Inf*ones(5)],
-                 [ones(3); Inf*ones(4); collect(2:4)],
-                 name="Unconstrained example").meta)
-print(IOBuffer(), ADNLPModel(x->0, zeros(10), x->[0.0;0.0;0.0], [0.0;0.0;-Inf],
-                 [Inf;0.0;0.0], name="Constrained example").meta)
-
-# A problem with zero variables doesn't make sense.
-@test_throws(ErrorException, NLPModelMeta(0))
-
-# Default methods should throw MethodError since they're not defined
-mutable struct DummyModel <: AbstractNLPModel
-  meta :: NLPModelMeta
-end
-model = DummyModel(NLPModelMeta(1))
-@test_throws(MethodError, lagscale(model, 1.0))
-for meth in [:obj, :varscale, :conscale]
-  @eval @test_throws(MethodError, $meth(model, [0.0]))
-end
-for meth in [:jac_structure!, :hess_structure!]
-  @eval @test_throws(MethodError, $meth(model, [0], [1]))
-end
-for meth in [:grad!, :cons!, :jac_coord!]
-  @eval @test_throws(MethodError, $meth(model, [0.0], [1.0]))
-end
-for meth in [:jth_con, :jth_congrad, :jth_sparse_congrad]
-  @eval @test_throws(MethodError, $meth(model, [0.0], 1))
-end
-@test_throws(MethodError, jth_congrad!(model, [0.0], 1, [2.0]))
-for meth in [:jprod!, :jtprod!]
-  @eval @test_throws(MethodError, $meth(model, [0.0], [1.0], [2.0]))
-end
-@test_throws(MethodError, jth_hprod(model, [0.0], [1.0], 2))
-@test_throws(MethodError, jth_hprod!(model, [0.0], [1.0], 2, [3.0]))
-for meth in [:ghjvprod!]
-  @eval @test_throws(MethodError, $meth(model, [0.0], [1.0], [2.0], [3.0]))
-end
-@assert isa(hess_op(model, [0.]), LinearOperator)
-@assert isa(jac_op(model, [0.]), LinearOperator)
-
-# ADNLPModel with no functions
-model = ADNLPModel(x->dot(x,x), zeros(2), name="square")
-@assert model.meta.name == "square"
-
-model = genrose_autodiff()
-for counter in fieldnames(typeof(model.counters))
-  @eval @assert $counter(model) == 0
+@testset "NLP tests" begin
+  for p in NLPModelsTest.nlp_problems
+    @testset "Problem $p" begin
+      nlp = eval(Symbol(p))()
+      @testset "Consistency" begin
+        consistent_nlps([nlp, nlp])
+      end
+      @testset "Check dimensions" begin
+        check_nlp_dimensions(nlp)
+      end
+      @testset "Multiple precision support" begin
+        multiple_precision_nlp(nlp)
+      end
+      @testset "View subarray" begin
+        view_subarray_nlp(nlp)
+      end
+      @testset "Test coord memory" begin
+        coord_memory_nlp(nlp)
+      end
+    end
+  end
 end
 
-obj(model, model.meta.x0)
-@assert neval_obj(model) == 1
-
-reset!(model)
-@assert neval_obj(model) == 0
-
-@test_throws(MethodError, jth_con(model, model.meta.x0, 1))
-include("test_tools.jl")
-
-include("test_slack_model.jl")
-include("test_qn_model.jl")
-include("nlp_testutils.jl")
-include("nls_testutils.jl")
-include("test_autodiff_model.jl")
-include("test_nlsmodels.jl")
-include("test_feasibility_form_nls.jl")
+@testset "NLS tests" begin
+  for p in NLPModelsTest.nls_problems
+    @testset "Problem $p" begin
+      nls = eval(Symbol(p))()
+      @testset "Consistency" begin
+        consistent_nlss([nls, nls])
+      end
+      @testset "Check dimensions" begin
+        check_nls_dimensions(nls)
+      end
+      @testset "Multiple precision support" begin
+        multiple_precision_nls(nls)
+      end
+      @testset "View subarray" begin
+        view_subarray_nls(nls)
+      end
+    end
+  end
+end
