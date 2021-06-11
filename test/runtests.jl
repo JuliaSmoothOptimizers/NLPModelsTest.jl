526d34db70cce6bbb9a5ee6559d306e8a55058f2
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-06-11 11:33:57 -0300

    Distributed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,45 +1,51 @@
-using NLPModels, NLPModelsTest, Test
+using Distributed
 
-@testset "NLP tests" begin
-  for p in NLPModelsTest.nlp_problems
-    @testset "Problem $p" begin
-      nlp = eval(Symbol(p))()
-      @testset "Consistency" begin
-        consistent_nlps([nlp, nlp], exclude = [])
-      end
-      @testset "Check dimensions" begin
-        check_nlp_dimensions(nlp, exclude = [])
-      end
-      @testset "Multiple precision support" begin
-        multiple_precision_nlp(nlp, exclude = [])
-      end
-      @testset "View subarray" begin
-        view_subarray_nlp(nlp, exclude = [])
-      end
-      @testset "Test coord memory" begin
-        coord_memory_nlp(nlp, exclude = [])
-      end
+np = Sys.CPU_THREADS
+addprocs(np - 1)
+
+@everywhere using NLPModels, NLPModelsTest, Test
+
+@everywhere function nlp_tests(p)
+  @testset "NLP tests of problem $p" begin
+    nlp = eval(Symbol(p))()
+    @testset "Consistency of problem $p" begin
+      consistent_nlps([nlp, nlp], exclude = [])
+    end
+    @testset "Check dimensions of problem $p" begin
+      check_nlp_dimensions(nlp, exclude = [])
+    end
+    @testset "Multiple precision support of problem $p" begin
+      multiple_precision_nlp(nlp, exclude = [])
+    end
+    @testset "View subarray of problem $p" begin
+      view_subarray_nlp(nlp, exclude = [])
+    end
+    @testset "Test coord memory of problem $p" begin
+      coord_memory_nlp(nlp, exclude = [])
     end
   end
 end
 
-@testset "NLS tests" begin
-  for p in NLPModelsTest.nls_problems
-    @testset "Problem $p" begin
-      nls = eval(Symbol(p))()
-      exclude = p == "LLS" ? [hess_coord, hess] : []
-      @testset "Consistency" begin
-        consistent_nlss([nls, nls], exclude = exclude)
-      end
-      @testset "Check dimensions" begin
-        check_nls_dimensions(nls, exclude = exclude)
-      end
-      @testset "Multiple precision support" begin
-        multiple_precision_nls(nls, exclude = exclude)
-      end
-      @testset "View subarray" begin
-        view_subarray_nls(nls, exclude = exclude)
-      end
+@everywhere function nls_tests(p)
+  @testset "NLS tests of problem $p" begin
+    nls = eval(Symbol(p))()
+    exclude = p == "LLS" ? [hess_coord, hess] : []
+    @testset "Consistency of problem $p" begin
+      consistent_nlss([nls, nls], exclude = exclude)
+    end
+    @testset "Check dimensions of problem $p" begin
+      check_nls_dimensions(nls, exclude = exclude)
+    end
+    @testset "Multiple precision support of problem $p" begin
+      multiple_precision_nls(nls, exclude = exclude)
+    end
+    @testset "View subarray of problem $p" begin
+      view_subarray_nls(nls, exclude = exclude)
     end
   end
 end
+
+pmap(nlp_tests, NLPModelsTest.nlp_problems)
+pmap(nls_tests, NLPModelsTest.nls_problems)
+
+rmprocs()
