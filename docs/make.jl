6c60e4bbb10f116e2d9d9cf9539c13259b493c79
abelsiqueira <abelsiqueira@users.noreply.github.com>
2021-07-10 21:27:17 +0000

    :robot: Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -13,4 +13,8 @@ makedocs(
   pages = ["Home" => "index.md", "Problems" => "problems.md", "Reference" => "reference.md"],
 )
 
-deploydocs(repo = "github.com/JuliaSmoothOptimizers/NLPModelsTest.jl.git", push_preview = true, devbranch = "main")
+deploydocs(
+  repo = "github.com/JuliaSmoothOptimizers/NLPModelsTest.jl.git",
+  push_preview = true,
+  devbranch = "main",
+)
